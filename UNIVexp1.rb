3fd2ef590dc8b6025988afaef640f35ae9761eb6
Rafal Daniel Szpecht <rufus111296@gmail.com>
2017-10-23 12:34:11 +0200

    Update UNIVexp1.rb

--- UNIVexp1.rb
+++ UNIVexp1.rb
@@ -1,8 +1,17 @@
 #                            POINT
 class Point
+    
   def initialize(x, y)
     @x = x
     @y = y
+  end
+  
+  def getx
+      return @x
+  end
+  
+  def gety
+      return @y
   end
   
   def show
@@ -14,8 +23,8 @@ class Point
   end
   
   def dist_between(p)
-    a = (p.x - @x).abs
-    b = (p.y - @y).abs 
+    a = (p.getx - @x).abs
+    b = (p.gety - @y).abs 
     return (Math.sqrt(a**2 + b**2))
   end
 end
