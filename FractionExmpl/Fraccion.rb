3350301b5e1ec11fbae7d1386a52dc5caf74fd09
Rafal Daniel Szpecht <rufus111296@gmail.com>
2017-10-24 17:11:37 +0200

    Update Fraccion.rb

--- FractionExmpl/Fraccion.rb
+++ FractionExmpl/Fraccion.rb
@@ -26,3 +26,34 @@ class Fraccion
       (@num*f.den) - (f.num*@den)
     end
 end
+
+
+# OR -------------------------------->
+class Fraccion
+  def initialize(num,den)
+    @num, @den = num, den
+  end
+  
+  attr_reader :num, :den
+  
+  def to_s
+    @num.to_s + '/' + @den.to_s
+  end
+  
+  def is_less?(other)
+    return self < other
+  end
+  
+  def is_grater?(other)
+    return self > other
+  end
+  
+  def is_equal?(other)
+    return self == other
+  end
+  
+  def <=> (other)
+    (@num*other.den) <=> (other.num*@den)
+  end
+  #Under test
+  
